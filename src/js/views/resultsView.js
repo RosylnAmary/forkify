d63b31d0b307c39e2392418e75e49eef6108165a
RosylnAmary <alicjaabr@gmail.com>
2024-04-26 12:26:54 +0200

    responsive v1.1

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -2,10 +2,11 @@ import View from './view';
 import previewView from './previewView';
 import icons from 'url:../../img/icons.svg';
 class ResultsView extends View {
-  _hideResults = document.querySelector('.btn--close-searchResults');
-  _showResults = document.querySelector('.show-results');
   _parentElement = document.querySelector('.results');
   _parentDivElement = document.querySelector('.search-results');
+  _hideResults = document.querySelector('.btn--close-searchResults');
+  _showResults = document.querySelector('.show-results');
+  _previewLink = document.querySelector('.preview__link');
   _errorMessage = 'No recipes found for your query! Please try again';
   _message = ' ';
 
@@ -13,6 +14,7 @@ class ResultsView extends View {
     super();
     this._addHandlerHideResults();
     this._addHandlerShowResults();
+    this._addHandlerShowRecipe();
   }
   _generateMarkup() {
     return this._data.map(result => previewView.render(result, false)).join('');
@@ -20,7 +22,14 @@ class ResultsView extends View {
   _addHandlerHideResults() {
     this._hideResults.addEventListener('click', this.closeResults.bind(this));
   }
+  _addHandlerShowRecipe() {
+    this._parentDivElement.addEventListener(
+      'click',
+      this.closeResults.bind(this)
+    );
+  }
   closeResults() {
+    console.log(this);
     this._parentDivElement.classList.remove('search-results-open');
     this._showResults.classList.remove('show-results-hide');
   }
@@ -29,7 +38,6 @@ class ResultsView extends View {
   }
   showResults() {
     this._parentDivElement.classList.add('search-results-open');
-    console.log('aa');
   }
 }
 
